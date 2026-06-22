618e1b3e88641d62f63ae3cfad5ab1c38e827d9d
Dipu <sadhanaroy198421@gmail.com>
2026-06-22 14:33:50 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -13,13 +13,13 @@ def _getenv(name: str, default: str | None = None, required: bool = False) -> st
     return val
     
 MUST_JOIN_CHANNEL = "-1003972430511"
-BOT_TOKEN = _getenv("BOT_TOKEN", required=True)
+BOT_TOKEN = _getenv("BOT_TOKEN", "8846137972:AAGYolSduNzMPEmEmZtkdl6L-4kvBkq-4ic")
 ADMIN_IDS = [int(i) for i in _getenv("ADMIN_IDS", "8619127196", required=True).replace(" ", "").split(",") if i]
-API_ID = "21377358"
-API_HASH = "e05bc1f4f03839db7864a99dbf72d1cd"
+API_ID = "37313295"
+API_HASH = "1f050e79d114ec31c85dc54a4d9b8cb7"
 
 DATABASE_URL = _getenv("DATABASE_URL")
-
+MONGO_URI = "mongodb+srv://TeleVault:TeleVault@televault.6kr2f9n.mongodb.net"
 
 
 DEFAULT_CURRENCY = _getenv("DEFAULT_CURRENCY", "₹")
